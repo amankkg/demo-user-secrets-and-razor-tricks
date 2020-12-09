b5a077ebb2d9b604f6ddb1abd96bf55da558018b
amankkg <amankubanychbek@gmail.com>
2020-12-09 21:27:41 +0600

    Add project files.

---
+++ Data/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class AppContext : DbContext
+    {
+        readonly string connectionString;
+
+        public AppContext(string connString)
+        {
+            connectionString = connString;
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ DemoWebApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DemoWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Data;
+
+namespace DemoWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppContext appContext;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            appContext = new AppContext(configuration["ConnectionString"]);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var todos = await appContext.Todos.ToListAsync();
+
+            return View(todos);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
